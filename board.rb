6b3cfb3a48028d393296b68ca4ce0b6e24f9b1b9
App Academy Student <student@appacademy.io>
2015-11-02 13:48:01 -0800

    completed #move in Board.rb

--- board.rb
+++ board.rb
@@ -6,16 +6,30 @@ class Board
 
   def initialize(grid = Array.new(8) {Array.new(8)} )
     @grid = grid
-    Board.populate
+    populate
   end
 
-  def self.populate
+  def [](pos)
+    @grid[pos[0]][pos[-1]]
+  end
+
+  def []=(pos, value)
+    @grid[pos[0]][pos[-1]] = value
+  end
+
+  def populate
     lines_with_pieces = [0,1,6,7]
 
     lines_with_pieces.each do |line|
-      line.each do |pos|
-        pos << Piece.new("pawn")
+      @grid[line].each_index do |i|
+        self[[line, i]] = Piece.new("pawn", [line, i])
       end
     end
+  end
+
+  def move(start, end_pos)
+    self[end_pos]= self[start]
+    self[end_pos].pos = end_pos
+    self[start] = nil
   end
 end
--- piece.rb
+++ piece.rb
@@ -1,8 +1,12 @@
 
 class Piece
 
+  attr_reader :type
+  attr_accessor :pos
 
-  def initialize(type)
+  def initialize(type, pos)
     @type = type
+    @pos = pos
   end
+
 end
